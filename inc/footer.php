b1af3cedbdef256cb0b4cc3052aa1d87090ea358
Guillaume Offré <offreguillaume@hotmail.fr>
2015-02-04 17:14:11 +0100

    Amélioration de la page d'accueil

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,10 @@
-<!-- JavaScript -->
-<script src="js/jquery-1.10.2.js"></script>
-<script src="js/bootstrap.js"></script>
+		<!-- JavaScript -->
+		<script src="js/jquery-1.10.2.js"></script>
+		<script src="js/bootstrap.js"></script>
 
-<footer>
-    <hr />
-    <center>2011-2015 - Projet Augustine</center>
-</footer>
-</body>
+		<footer>
+    	<hr />
+    	<center>2011-2015 - Projet Augustine</center>
+		</footer>
+	</body>
+</html>
